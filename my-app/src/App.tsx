0deeb66498c657c4a80ede5ed37f39393a6fa897
maierhoferma81177@th-nuernberg.de <mmaierhofer@outlook.de>
2022-02-19 16:48:12 +0100

    add auth page

--- my-app/src/App.tsx
+++ my-app/src/App.tsx
@@ -1,20 +1,34 @@
-import React from "react";
+import React, { useState } from "react";
 import "./App.css";
 import Footer from "./layouts/Footer";
 import Header from "./layouts/Header";
 import Main from "./layouts/Main";
 import { BrowserRouter as Router } from "react-router-dom";
+import Authentication from "./pages/Authentication";
 
 function App() {
-  return (
-    <div className="App">
-      <Router>
-        <Header />
-        <Main />
-        <Footer />
-      </Router>
-    </div>
-  );
+  const isLoggedIn: boolean = false;
+
+  if (isLoggedIn) {
+    return (
+      <div className="App">
+        <Router>
+          <Header />
+          <Main />
+          <Footer />
+        </Router>
+      </div>
+    );
+  } else {
+    return (
+      <div className="App">
+        <Router>
+          <Authentication />
+          <Footer />
+        </Router>
+      </div>
+    );
+  }
 }
 
 export default App;
