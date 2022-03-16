dcffed87f4a5fe6ac8250fdb01705b9a0724171a
maierhoferma81177@th-nuernberg.de <mmaierhofer@outlook.de>
2022-03-16 18:51:52 +0100

    changes for changes

--- my-app/src/App.tsx
+++ my-app/src/App.tsx
@@ -7,7 +7,7 @@ import { BrowserRouter as Router } from "react-router-dom";
 import Authentication from "./pages/Authentication";
 import { UserContext } from "./contexts/userContext";
 import { initializeApp } from "firebase/app";
-import { getAuth, signInWithEmailAndPassword } from "firebase/auth";
+import { getAuth } from "firebase/auth";
 import { firebaseConfig } from "./config/firebase";
 
 interface users {
@@ -23,11 +23,12 @@ interface auth {
 }
 
 function App() {
+  console.log("change");
   const auth = getAuth();
 
   const app = initializeApp(firebaseConfig);
 
-  if (auth.users.length > 0) {
+  if (auth) {
     return (
       <div className="App">
         <Router>
