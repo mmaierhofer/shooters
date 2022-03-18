f5d3c18181b8f2f8f90f98d7bc48e6409121f4c1
maierhoferma81177@th-nuernberg.de <mmaierhofer@outlook.de>
2022-03-18 20:54:49 +0100

    add searchbar

--- my-app/src/components/DropDownMenu/Searchbar/index.tsx
+++ my-app/src/components/DropDownMenu/Searchbar/index.tsx
@@ -1,5 +1,20 @@
 import React from "react";
 
 export default function Searchbar() {
-  return <></>;
+  return (
+    <>
+      <div>
+        <form className="bg-white shadow-md rounded px-8 pt-6 pb-8 mb-4 max-h-20">
+          <div className="mb-4">
+            <input
+              className="shadow appearance-none border rounded w-full py-2 px-3 text-gray-700 leading-tight focus:outline-none focus:shadow-outline"
+              id="username"
+              type="text"
+              placeholder="Username"
+            />
+          </div>
+        </form>
+      </div>
+    </>
+  );
 }
--- my-app/src/layouts/Header/index.tsx
+++ my-app/src/layouts/Header/index.tsx
@@ -1,51 +1,15 @@
 import * as React from "react";
-import AppBar from "@mui/material/AppBar";
-import Box from "@mui/material/Box";
-import Toolbar from "@mui/material/Toolbar";
-import Typography from "@mui/material/Typography";
-import IconButton from "@mui/material/IconButton";
-import AccountCircle from "@mui/icons-material/AccountCircle";
-import AddIcon from "@mui/icons-material/Add";
-import MenuItem from "@mui/material/MenuItem";
-import LocationOnIcon from "@mui/icons-material/LocationOn";
-import FormatListBulletedIcon from "@mui/icons-material/FormatListBulleted";
-import Menu from "@mui/material/Menu";
-import DropDownMenu from "../../components/DropDownMenu";
+import Searchbar from "../../components/DropDownMenu/Searchbar";
 
 export default function Header() {
-  const [auth, setAuth] = React.useState(true);
-  const [anchorEl, setAnchorEl] = React.useState<null | HTMLElement>(null);
-
-  const handleMenu = (event: React.MouseEvent<HTMLElement>) => {
-    setAnchorEl(event.currentTarget);
-  };
-
-  const handleClose = () => {
-    setAnchorEl(null);
-  };
-
-  const menuItems = [
-    {
-      name: "Add Workshop",
-      route: "/workshop/add",
-      icon: <AddIcon />,
-    },
-    {
-      name: "My Workshops",
-      route: "/workshops",
-      icon: <FormatListBulletedIcon />,
-    },
-    {
-      name: "My Locations",
-      route: "/locations",
-      icon: <LocationOnIcon />,
-    },
-  ];
-
   return (
-    <div className="absolute h-20 bg-white w-screen flex flex-row">
+    <div className="absolute max-h-20 h-20 bg-white w-screen flex flex-row">
       <div className="w-44 border-b z-50"></div>
-      <div className="bg-white shadow-lg flex-grow z-0"></div>
+      <div className="bg-white shadow-lg flex-grow z-0">
+        <div className="w-3/4">
+          <Searchbar />
+        </div>
+      </div>
     </div>
   );
 }
