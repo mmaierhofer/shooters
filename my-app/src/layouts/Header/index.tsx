1ed7d5bef2b0386bad414407925cfcd1941b6d32
maierhoferma81177@th-nuernberg.de <mmaierhofer@outlook.de>
2022-03-19 19:32:27 +0100

    make searchbar smaller

--- my-app/src/layouts/Header/index.tsx
+++ my-app/src/layouts/Header/index.tsx
@@ -6,7 +6,7 @@ export default function Header() {
     <div className="absolute max-h-20 h-20 bg-white w-screen flex flex-row">
       <div className="w-44 border-b z-50"></div>
       <div className="bg-white shadow-lg flex-grow z-0">
-        <div className="w-3/4">
+        <div className="w-1/2">
           <Searchbar />
         </div>
       </div>
